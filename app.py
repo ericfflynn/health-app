8c5dbbd0a86646bada030b0ddc7c0739ea42557f
Eric J Flynn <ejflynn27@gmail.com>
2022-10-28 14:50:39 -0400

    visual changes

--- app.py
+++ app.py
@@ -4,13 +4,20 @@ import pandas as pd
 import datetime
 import plotly.express as px
 import os 
+import altair as alt
 
 st.set_page_config(
-    page_title="Fitness Dashboard",
+    page_title="Fitness Dashboard: Eric Flynn",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="expanded",  
 )
-
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
 hide_table_row_index = """
             <style>
             thead tr th:first-child {display:none}
@@ -32,11 +39,21 @@ def get_data():
 	df['Duration'] = df['Duration'].apply(lambda x: str(x)[-8:])
 	df.sort_values(by=['start'], ascending=False, inplace=True)
 	df.reset_index(inplace=True, drop=True)
-	df.rename(columns={'name':'Workout Type','start':'Date','activeEnergy':'Calories','avgHR':"Avg BPM"},inplace=True)
+	df.rename(columns={'name':'Workout Type','start':'Date','activeEnergy':'Calories','avgHR':'Avg BPM','maxHR':'Max BPM'},inplace=True)
 	for i in ['Calories','Avg BPM']:
 		df[i] = df[i].apply(lambda x: int(x))
 	df['Month'] = df['start_dt'].apply(lambda x: x.strftime("%Y-%m"))
+	def f(row):
+		if row['Workout Type'] in ['Traditional Strength Training', 'Functional Strength Training'] :
+			val = 'Strength Training'
+		elif row['Workout Type'] in ['Tennis','Golf']:
+			val = 'Sport'
+		else:
+			val = 'Cardio/Core'
+		return val
+	df['Category'] = df.apply(f,axis=1)
 	return df
+
 
 df = get_data()
 
@@ -44,55 +61,35 @@ st.title("Personal Fitness Dashboard")
 st.markdown("#")
 types = pd.concat([pd.Series(['Select All']), df['Workout Type'].drop_duplicates()])
 types_choice = st.sidebar.selectbox('Workout Type',types)
-start_date = st.sidebar.date_input('First Date',min(df['start_dt']).to_pydatetime())
-end_date = st.sidebar.date_input('Last Date',datetime.datetime.today())
+start_date = st.sidebar.date_input('Start Date',min(df['start_dt']).to_pydatetime())
+end_date = st.sidebar.date_input('End Date',datetime.datetime.today())
 
 config = {'displayModeBar': False}
 
 if types_choice == 'Select All':
 	dfdis = df.query("start_dt >= @start_date & end_dt <= @end_date")
-	totalworkouts = len(dfdis)
-	maxhr = dfdis['maxHR'].max()
-	avgcalories = dfdis['Calories'].mean()
-	avghr = dfdis['Avg BPM'].mean()
-	dfbar = dfdis.groupby(['Month']).agg({'Workout Type':'count'}).rename(columns={'Workout Type':'# Workouts'})
-	bar_chart = px.bar(dfbar, x= dfbar.index, y='# Workouts')
-	col1_1, col2_1, col3_1, col4_1 = st.columns(4)
-	with col1_1:
-		st.metric(label='Total Workouts', value = int(totalworkouts))
-	with col2_1:
-		st.metric(label='Max BPM', value = maxhr)
-	with col3_1:
-		st.metric(label='Avg Calories', value = int(avgcalories))
-	with col4_1:
-		st.metric(label='Avg BPM', value = int(avghr))
-	st.markdown("#")
-	col1_2, col2_2 = st.columns(2)
-	with col1_2:
-		st.write(bar_chart, config=config)
-	with col2_2:
-		st.dataframe(dfdis[['Workout Type','Date','Duration','Avg BPM','Calories']])
 else:
-	dfdis = df.query("start_dt >= @start_date & end_dt <= @end_date & `Workout Type` == @types_choice")
-	totalworkouts = len(dfdis)
-	maxhr = dfdis['maxHR'].max()
-	avgcalories = dfdis['Calories'].mean()
-	avghr = dfdis['Avg BPM'].mean()
-	dfbar = dfdis.groupby(['Month']).agg({'Workout Type':'count'}).rename(columns={'Workout Type':'# Workouts'})
-	bar_chart = px.bar(dfbar, x= dfbar.index, y='# Workouts')
-	col1_1, col2_1, col3_1, col4_1 = st.columns(4)
-	with col1_1:
-		st.metric(label='Total Workouts', value = int(totalworkouts))
-	with col2_1:
-		st.metric(label='Max BPM', value = maxhr)
-	with col3_1:
-		st.metric(label='Avg Calories', value = int(avgcalories))
-	with col4_1:
-		st.metric(label='Avg BPM', value = int(avghr))
-	st.markdown("#")
-	col1_2, col2_2 = st.columns(2)
-	with col1_2:
-		st.write(bar_chart, config=config)
-	with col2_2:
-		st.dataframe(dfdis[['Workout Type','Date','Duration','Avg BPM','Calories']])
+	dfdis = df.query("start_dt >= @start_date & end_dt <= @end_date & `Workout Type` == @types_choice")	
 
+totalworkouts = len(dfdis)
+maxhr = dfdis['Max BPM'].max()
+avgcalories = dfdis['Calories'].mean()
+avghr = dfdis['Avg BPM'].mean()
+
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+	st.metric(label='Total Workouts', value = int(totalworkouts))
+with col2:
+	st.metric(label='Max BPM', value = maxhr)
+with col3:
+	st.metric(label='Avg Calories', value = int(avgcalories))
+with col4:
+	st.metric(label='Avg BPM', value = int(avghr))
+
+altbar = alt.Chart(dfdis).mark_bar().encode(
+	x='Month:T',
+	y=alt.Y(field='Workout Type',aggregate='count',type='quantitative', title='# Workouts')
+	)
+
+st.altair_chart(altbar, use_container_width=True)
+st.write(dfdis[['Category','Workout Type','Date','Duration','Calories','Avg BPM','Max BPM']],use_container_width=True)
