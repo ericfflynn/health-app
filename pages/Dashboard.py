48b7d277b1f8d69e5c767793b48a199cea04cd0e
Eric J Flynn <ejflynn27@gmail.com>
2022-12-18 13:31:18 -0500

    phase 2 additions

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -5,12 +5,14 @@ import datetime
 import plotly.express as px
 import os 
 import altair as alt
+import pymysql
 
 st.set_page_config(
     page_title="Fitness Dashboard: Eric Flynn",
     layout="wide",
     initial_sidebar_state="expanded",  
 )
+
 hide_default_format = """
        <style>
        #MainMenu {visibility: hidden; }
@@ -26,6 +28,15 @@ hide_table_row_index = """
             """
 key = os.environ.get('KEY')
 headers = {"x-api-key":key}
+
+conn = pymysql.connect(
+	host='database-1.cw1pd3mihy4s.us-east-2.rds.amazonaws.com',
+    user='admin',
+    password='mysqldbpw',
+    db='Health_Data'
+	)
+cursor = conn.cursor()
+
 
 @st.cache(ttl=21600)
 def get_data():
@@ -54,28 +65,37 @@ def get_data():
 	df['Category'] = df.apply(f,axis=1)
 	return df
 
+# def get_data():
+# 	query = "SELECT name, start, end, `activeEnergy.qty`, `avgHeartRate.qty`, `maxHeartRate.qty`, BodyPart, FROM Workouts"
+# 	df = pd.read_sql(query, conn)
+# 	for i in ['start','end']:
+# 		df[i+'_dt'] = df[i].apply(lambda x: datetime.datetime.strptime(x,"%Y-%m-%d %H:%M:%S"))
+# 		df[i] = df[i].apply(lambda x: x[0:10])
+# 	df['Duration'] = df['end_dt'] - df['start_dt']
+# 	df['Duration'] = df['Duration'].apply(lambda x: str(x)[-8:])
+# 	df.sort_values(by=['start'], ascending=False, inplace=True)
+# 	df.reset_index(inplace=True, drop=True)
+# 	df.rename(columns={'name':'Workout Type','start':'Date','activeEnergy':'Calories','avgHR':'Avg BPM','maxHR':'Max BPM'},inplace=True)
+# 	for i in ['Calories','Avg BPM']:
+# 		df[i] = df[i].apply(lambda x: int(x))
+# 	df['Month'] = df['start_dt'].apply(lambda x: x.strftime("%Y-%m"))
+# 	def f(row):
+# 		if row['Workout Type'] in ['Traditional Strength Training', 'Functional Strength Training'] :
+# 			val = 'Strength Training'
+# 		elif row['Workout Type'] in ['Tennis','Golf']:
+# 			val = 'Sport'
+# 		else:
+# 			val = 'Cardio/Core'
+# 		return val
+# 	df['Category'] = df.apply(f,axis=1)
+# 	return df
 df = get_data()
 
-st.title("Personal Fitness Dashboard")
 
-# cats = pd.concat([pd.Series(['Select All']), df['Category'].drop_duplicates()])
-# cats_choice = st.sidebar.selectbox('Workout Category',cats)
-# if cats_choice == 'Select All':
-# 	types = pd.concat([pd.Series(['Select All']), df['Workout Type'].drop_duplicates()])
-# 	types_choice = st.sidebar.selectbox('Workout Type',types)
-# 	start_date = st.sidebar.date_input('Start Date',min(df['start_dt']).to_pydatetime())
-# 	end_date = st.sidebar.date_input('End Date',datetime.datetime.today())
-# 	if types_choice =='Select All':
-# 		dfdis = df.query("start_dt >= @start_date & end_dt <= @end_date")
-# 	else:
-# 		dfdis = df.query("start_dt >= @start_date & end_dt <= @end_date & 'Workout Type' == @types_choice")	
-# else:
-# 	types = pd.concat([pd.Series(['Select All']),df.query("`Category` == @cats_choice")['Workout Type'].drop_duplicates()])
-# 	types_choice = st.sidebar.selectbox('Workout Type',types)
-# 	start_date = st.sidebar.date_input('Start Date',min(df['start_dt']).to_pydatetime())
-# 	end_date = st.sidebar.date_input('End Date',datetime.datetime.today())
-# 	dfdis = df.query("start_dt >= @start_date & end_dt <= @end_date & `Category` == @cats_choice & 'Workout Type' == @types_choice")	
+st.title("Dashboard")	
 
+st.markdown("This page is the dashboard itself. Please play around with the slicers on the left hand side to modify the date range and workout type.")
+st.write('***')
 types = pd.concat([pd.Series(['Select All']), df['Workout Type'].drop_duplicates()])
 types_choice = st.sidebar.selectbox('Workout Type',types)
 start_date = st.sidebar.date_input('Start Date',min(df['start_dt']).to_pydatetime())
